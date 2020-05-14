37fdb599b65a1d03105fe45d1363db7053de2473
Bruno Giacobo Pinto <bruno.pinto@paysmart.com.br>
2020-05-14 15:28:00 -0300

    feat(plugin): update IDid sdk version
    update IDid sdk version

--- android/src/main/kotlin/br/com/useidid/idid_flutter/IdidFlutterPlugin.kt
+++ android/src/main/kotlin/br/com/useidid/idid_flutter/IdidFlutterPlugin.kt
@@ -16,15 +16,6 @@ class IdidFlutterPlugin : FlutterPlugin, MethodCallHandler {
         channel.setMethodCallHandler(IdidFlutterPlugin());
     }
 
-    // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-    // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-    // plugin registration via this function while apps migrate to use the new Android APIs
-    // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-    //
-    // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-    // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-    // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-    // in the same class.
     companion object {
         @JvmStatic
         fun registerWith(registrar: Registrar) {
@@ -40,10 +31,11 @@ class IdidFlutterPlugin : FlutterPlugin, MethodCallHandler {
         }
     }
 
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    }
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) = Unit
 
     private fun provision(call: MethodCall, result: Result) {}
 
-    private fun authorize(call: MethodCall, result: Result) {}
+    private fun authorize(call: MethodCall, result: Result) {
+        //IDidAuth.instance.authorize()
+    }
 }
