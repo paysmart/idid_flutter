c8597839cbc9df43bb30c253bc4e048634227279
Bruno Giacobo Pinto <bruno.pinto@paysmart.com.br>
2020-06-16 13:42:07 -0300

    feat(plugin): use new provisioning
    Use new provisioning

--- android/src/main/kotlin/br/com/useidid/idid_flutter/IdidFlutterPlugin.kt
+++ android/src/main/kotlin/br/com/useidid/idid_flutter/IdidFlutterPlugin.kt
@@ -4,6 +4,12 @@ import android.content.Context
 import android.util.Log
 import androidx.annotation.NonNull
 import br.com.idid.sdk.IDidAuth
+import br.com.idid.sdk.IDidProvisionCallback
+import br.com.idid.sdk.messages.IDidProvisionFailed
+import br.com.idid.sdk.messages.IDidProvisionPayload
+import br.com.idid.sdk.messages.IDidProvisionSucceed
+import br.com.idid.sdk.models.IDidDeliveryAddress
+import br.com.idid.sdk.provisioning.IDidDataPrepSpec
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -37,7 +43,48 @@ class IdidFlutterPlugin(private val mContext: Context) : FlutterPlugin, MethodCa
     }
 
     private fun provision(call: MethodCall, result: Result) {
-        Log.d("____", "Provision called")
+
+        val payload = IDidProvisionPayload(
+                userName = call.argument<String>("name")!!,
+                userEmail = call.argument<String>("email")!!,
+                issuerId = call.argument<String>("issuerId")!!,
+                phoneNumber = call.argument<String>("phoneNumber")!!,
+                documentId = call.argument<String>("documentId")!!,
+                address = IDidDeliveryAddress(
+                        streetAddress = "Manoelito de Ornelas",
+                        neighborhood = "Praia de Belas",
+                        complement = "Paysmart Matrix",
+                        city = "Porto Alegre",
+                        zipcode = "90110230",
+                        streetNumber = "55",
+                        country = "Brazil",
+                        state = "RS"
+                ),
+                dataPrep = IDidDataPrepSpec(
+                        mKDI = 2,
+                        mScheme = "ELO",
+                        mProductType = "Debit",
+                        mPANSequence = 0,
+                        mDerivationKey = call.argument<String>("derivationKey")!!,
+                        mTrack2EqData = call.argument<String>("track2")!!,
+                        mExpirationDate = 1735576301
+                )
+        )
+
+        val callback = object : IDidProvisionCallback(payload) {
+            override fun onError(error: IDidProvisionFailed?) {
+                Log.d("____", "Provision Failed ")
+                result.success("fail")
+            }
+
+            override fun onSuccess(value: IDidProvisionSucceed?) {
+                Log.d("____", "Provision Succeed ")
+                result.success("succeed")
+            }
+        }
+
+        IDidAuth.getInstance(mContext)
+                .provision(callback)
     }
 
     private fun authorize(call: MethodCall, result: Result) {
