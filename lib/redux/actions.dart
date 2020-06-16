9cdca295066e5c8c1680e36e96f4a907346b2f9c
Bruno Giacobo Pinto <bruno.pinto@paysmart.com.br>
2020-06-16 14:51:15 -0300

    feat(plugin): add provision message structure
    Add provision message structure

--- lib/redux/actions.dart
+++ lib/redux/actions.dart
@@ -8,5 +8,24 @@ class AuthorizeTransactionAction {
 }
 
 class ProvisionAction {
-  Map<String, dynamic> toJson() => {};
+  final String name;
+  final String email;
+  final String track2;
+  final String issuerId;
+  final String documentId;
+  final String phoneNumber;
+  final String derivationKey;
+
+  ProvisionAction(this.name, this.email, this.track2, this.issuerId,
+      this.documentId, this.phoneNumber, this.derivationKey);
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'email': email,
+        'track2': track2,
+        'issuerId': issuerId,
+        'documentId': documentId,
+        'phoneNumber': phoneNumber,
+        'derivationKey': derivationKey
+      };
 }
