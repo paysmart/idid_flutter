505d665735b1ff53441c05a7c01464c30e66a213
Maurício Freitas <mauricio.freitas@softdesign.com.br>
2022-03-02 17:24:03 -0300

    fix(iOS): fix method channel name

--- ios/Classes/SwiftIdidFlutterPlugin.swift
+++ ios/Classes/SwiftIdidFlutterPlugin.swift
@@ -3,7 +3,7 @@ import UIKit
 
 public class SwiftIdidFlutterPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "idid_flutter", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "br.com.idid.sdk/idid_plugin_channel", binaryMessenger: registrar.messenger())
     let instance = SwiftIdidFlutterPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
